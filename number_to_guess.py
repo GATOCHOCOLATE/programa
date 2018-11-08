af348fa29aa752e1fd0905f54c724258fcd1ec2b
José Luis <jolutorre7@gmail.com>
2018-11-08 12:28:21 +0100

    Juego de adivinar número versión sencilla

--- number_to_guess.py
+++ number_to_guess.py
@@ -1,18 +1,20 @@
 
 print("Bienbenido al juego de adivinar un número entre el 0 y el 10")
 
-tryes = 5
+attempts = 5
 
 number_to_guess = 6
 
-while tryes > 0:
+user_number = None
+
+while attempts > 0:
     user_number = int(input("Numero: "))
     if user_number == number_to_guess:
-        tryes = 0
+        attempts = 0
         print("You Win")
     else:
-        tryes -= 1
-        print("Lo siento ese número no es, te quedan {} intentos".format(tryes))
+        attempts -= 1
+        print("Lo siento ese número no es, te quedan {} intentos".format(attempts))
 
 if not user_number == number_to_guess:
     print("You Lose")
